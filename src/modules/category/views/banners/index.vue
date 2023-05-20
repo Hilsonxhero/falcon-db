54aab8c08a288f2d67011274b57d4994e7fa5603
Hilson <hilsonxhero.me@gmail.com>
2023-05-20 14:37:39 +0330

    fixes

--- src/modules/category/views/banners/index.vue
+++ src/modules/category/views/banners/index.vue
@@ -81,6 +81,8 @@
 </template>
 
 <script setup lang="ts">
+// @ts-nocheck
+// @ts-ignore
 import { ref, onMounted, watch, watchEffect } from "vue";
 import HxDataTable from "@/components/common/datatable/DataTable.vue";
 import { HxMessageBox } from "@/components/base/message-box";
